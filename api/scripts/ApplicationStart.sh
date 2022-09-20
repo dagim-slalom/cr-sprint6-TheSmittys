22eb40ccc374e5491f14bd53fe26f98f6a6212cb
Dagim D <dagim.dawit17@gmail.com>
2022-09-20 11:12:14 -0700

    handling the server timeout error

--- api/scripts/ApplicationStart.sh
+++ api/scripts/ApplicationStart.sh
@@ -4,4 +4,8 @@
 sudo apt-get update
 
 # Run the API
-java -jar /home/ubuntu/java/target/*.war
+# java -jar /home/ubuntu/java/target/*.war
+
+#Start the server and run the API
+cd /home/ubuntu/java/target
+nohup java -jar *.war </dev/null &>/dev/null &
